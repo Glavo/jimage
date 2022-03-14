cc6b2af267ed96b1e2e9eb1c026f037baa5e2dae
Glavo <zjx001202@gmail.com>
2022-03-14 17:19:30 +0800

    Update README

---
+++ README.md
@@ -0,0 +1,23 @@
+# JImage 
+
+This library is used to read Jimage files on Java 8 or higher.
+It was extracted from the OpenJDK 17.0.2 source code, the public replacement for `jdk.internal.jimage`,
+and it works on Java 8 and above.
+
+It has been published to Maven Central. You can add dependencies on it in this way:
+
+Maven:
+```xml
+<dependency>
+  <groupId>org.glavo</groupId>
+  <artifactId>jimage</artifactId>
+  <version>1.0.0</version>
+</dependency>
+```
+
+Gradle:
+```kotlin
+dependencies {
+  implementation("org.glavo:jimage:1.0.0")
+}
+```
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
 import java.io.RandomAccessFile
+import java.util.Properties
 
 plugins {
-    id("java")
+    java
+    signing
+    `maven-publish`
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
 group = "org.glavo"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -15,6 +19,11 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
 
 tasks.compileJava {
@@ -35,6 +44,109 @@ tasks.compileJava {
     }
 }
 
+tasks.javadoc {
+    options.encoding = "UTF-8"
+    (options as StandardJavadocDocletOptions).apply {
+        encoding = "UTF-8"
+        tags = listOf(
+            "apiNote:a:API Note:",
+            "implSpec:a:Implementation Requirements:",
+            "implNote:a:Implementation Note:"
+        )
+
+        addStringOption("Xdoclint:none", "-quiet")
+    }
+
+}
+
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+tasks.withType<GenerateModuleMetadata>().configureEach {
+    enabled = false
+}
+
+configure<PublishingExtension> {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            version = project.version.toString()
+            artifactId = project.name
+            from(components["java"])
+
+            pom {
+                name.set(project.name)
+                description.set("Public distribution of jdk.internal.jimage")
+                url.set("https://github.com/Glavo/jimage")
+                licenses {
+                    license {
+                        name.set("GPL v2 with Classpath Exception")
+                        url.set("https://openjdk.java.net/legal/gplv2+ce.html")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("glavo")
+                        name.set("Glavo")
+                        email.set("zjx001202@gmail.com")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/Glavo/jimage")
+                }
+            }
+        }
+    }
+}
+
+var secretPropsFile = project.rootProject.file("gradle/maven-central-publish.properties")
+if (!secretPropsFile.exists()) {
+    secretPropsFile = file(System.getProperty("user.home")).resolve(".gradle").resolve("maven-central-publish.properties")
+}
+
+if (secretPropsFile.exists()) {
+    // Read local.properties file first if it exists
+    val p = Properties()
+    secretPropsFile.reader().use {
+        p.load(it)
+    }
+
+    p.forEach { (name, value) ->
+        rootProject.ext[name.toString()] = value
+    }
+}
+
+listOf(
+    "sonatypeUsername" to "OSSRH_USERNAME",
+    "sonatypePassword" to "OSSRH_PASSWORD",
+    "sonatypeStagingProfileId" to "SONATYPE_STAGING_PROFILE_ID",
+    "signing.keyId" to "SIGNING_KEY_ID",
+    "signing.password" to "SIGNING_PASSWORD",
+    "signing.key" to "SIGNING_KEY"
+).forEach { (p, e) ->
+    if (!rootProject.ext.has(p)) {
+        rootProject.ext[p] = System.getenv(e)
+    }
+}
+
+signing {
+    if (rootProject.ext.has("signing.key")) {
+        useInMemoryPgpKeys(
+            rootProject.ext["signing.keyId"].toString(),
+            rootProject.ext["signing.key"].toString(),
+            rootProject.ext["signing.password"].toString(),
+        )
+    }
+    sign(publishing.publications["maven"])
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            stagingProfileId.set(rootProject.ext["sonatypeStagingProfileId"].toString())
+            username.set(rootProject.ext["sonatypeUsername"].toString())
+            password.set(rootProject.ext["sonatypePassword"].toString())
+        }
+    }
 }
